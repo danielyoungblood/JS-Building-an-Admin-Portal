7ea10ba4003e4a56db3a068ef7d5c082caf045b1
daniel <danielyknechtli@gmail.com>
2023-04-20 19:04:48 -0700

    making comments

--- public/index.js
+++ public/index.js
@@ -1,28 +1,35 @@
 async function main() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let books = await response.json();
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
-
-    books.forEach(renderBook)
+  //Once the array of books is retrieved, we use .forEach to add cards for each book to the DOM using the renderBook function
+  books.forEach(renderBook);
 }
 
+//using the book (each element of the books array) variable, we then get a reference to the div element that will contain each information about each book
+//and finally keeping adding information about each book to the div element that will contain this infomation to all the books
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
+  let bookContainer = document.querySelector(".book-container");
+  bookContainer.innerHTML += `
         <div class="col-sm-3">
             <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
+                ${
+                  book.imageURL
+                    ? `
                     <img class="card-img-top" src="${book.imageURL}" />
                 `
-                : ``}
+                    : ``
+                }
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Available: ${
+                      book.quantity
+                    }</h6>
                     <p class="card-text">${book.description}</p>
                 </div>
             </div>
         </div>
-    `
+    `;
 }
 
-main()  //code exectution begins on this line, line 1-26 deals with defining functions that are not automatically executed
+main(); //code exectution begins on this line, line 1-33 deals with defining functions that are not automatically executed
